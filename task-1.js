f4dfe131b8f71e0b8f0f2488f3f4fc2e149fd152
Mark Doering <mark.doering.1995@gmail.com>
2024-10-24 13:30:54 +0200

    finished task

--- task-1.js
+++ task-1.js
@@ -1,19 +1,61 @@
 // Task 1
+const user1 = {
+    firstName: 'Mark',
+    lastName: 'Maxwell',
+    age: 32,
+    email: 'mark@email.com'
+  };
+  
+  const user2 = {
+    firstName: 'Sally',
+    lastName: 'Samson',
+    age: 24,
+    email: 'sally@email.com'
+  };
+  
+  const user3 = {
+    firstName: 'Jenny',
+    lastName: 'Jay',
+    age: 38,
+    email: 'jenny@email.com'
+  };
+  
+  console.log(user1);
+  console.log(user2);
+  console.log(user3);
+  
+// Task 1.1
 
-const user1 = {};  
-const user2 = {};  
-const user3 = {};  
+user1.city = 'New York';
+user2.city = 'Los Angeles';
+user3.city = 'Chicago';
 
 console.log(user1);
 console.log(user2);
 console.log(user3);
 
-// Task 1.1
 
 // Task 1.2
-
-function describe() {}
-
+const describe = (user) => {
+    return `${user.firstName} ${user.lastName} is ${user.age} years old and lives in ${user.city}.`;
+  };
+  
+  console.log(describe(user1));
+  console.log(describe(user2));
+  console.log(describe(user3));
+  
 // Task 1.3
+user1.job = 'Engineer';
+user2.job = 'Web Developer';
 
-function showJob() {}
+const showJob = (user) => {
+  if (user.job) {
+    return `${user.firstName} ${user.lastName} works as a ${user.job}.`;
+  } else {
+    return `${user.firstName} ${user.lastName} is currently unemployed.`;
+  }
+};
+
+console.log(showJob(user1)); 
+console.log(showJob(user2));
+console.log(showJob(user3)); 
--- task-2.js
+++ task-2.js
@@ -1,17 +1,33 @@
 // Task 2
-
-const vehicle1 = {};
-const vehicle2 = {};
-const vehicle3 = {};
-
-console.log(vehicle1);
-console.log(vehicle2);
-console.log(vehicle3);
-
+const vehicle1 = {
+    type: 'car',
+    color: 'red',
+    brand: 'Toyota',
+    mileage: 55000
+  };
+  
+  const vehicle2 = {
+    type: 'truck',
+    color: 'blue',
+    brand: 'Ford',
+    mileage: 75000
+  };
+  
+  const vehicle3 = {
+    type: 'motorcycle',
+    color: 'black',
+    brand: 'Honda',
+    mileage: 45000
+  };
+  console.log(vehicle1);
+  console.log(vehicle2);
+  console.log(vehicle3);
+  
 // Task 2.1
-
-function needsRevision() {}
-
-console.log(`Does vehicle1 need revision?`, needsRevision(vehicle1));
-console.log(`Does vehicle2 need revision?`, needsRevision(vehicle2));
-console.log(`Does vehicle3 need revision?`, needsRevision(vehicle3));
+const needsRevision = (vehicle) => {
+    return vehicle.mileage > 60000;
+  };
+  console.log(needsRevision(vehicle1));
+  console.log(needsRevision(vehicle2));
+  console.log(needsRevision(vehicle3));
+  
